37f02e82199ba4504f00d89fe8d7a795f6c7a2b6
Thaly2005 <thalyta.ferreira@alunoescolassociais.org.br>
2024-09-17 11:24:56 -0300

    Começa a construir o formulário de cadastro

--- views/register.php
+++ views/register.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuário</title>
+    <title>Cadastre-se</title>
 </head>
 <body>
-    
+    <div>
+        <h2>Cadastro de Usuário</h2>
+        <form action="" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome" required>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required>
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha" required>
+        </form>
+    </div>
 </body>
 </html>
