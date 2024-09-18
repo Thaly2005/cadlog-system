9b289c7804aa6eba30c3f6f286562b44edad8452
Thaly2005 <thalyta.ferreira@alunoescolassociais.org.br>
2024-09-18 07:31:51 -0300

    Finaliza o formulário de cadastro .

--- views/register.php
+++ views/register.php
@@ -22,7 +22,10 @@
                 <option value="gestor">Gestor</option>
                 <option value="colaborador">Colaborador</option>
             </select>
+
+            <button type="submit">Cadastrar</button>
         </form>
+        <a href="">Voltar ao Login</a>
     </div>
 </body>
 </html>
