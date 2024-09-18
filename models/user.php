aabcccf2ec9d0b73d5a34bd689abf68145886931
Thaly2005 <thalyta.ferreira@alunoescolassociais.org.br>
2024-09-18 08:03:12 -0300

    Configura o database.php

--- models/user.php
+++ models/user.php
@@ -1,2 +1,6 @@
 <?php
+
+// Inclui o arquivo de conexão que contem a classe Database para gerenciar a conexão com o BD
+require_once 'models/database.php';
+
 ?>
---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+// Inclui arquivos  de controlador necessarios para lidar com diferentes açoes
+require 'controllers/AuthController.php'; //  Instancia o controlador de autenticaçao
+require 'controllers/UserController.php'; // Instancia o controlador de usuario
+require 'controllers/DashboardController.php'; // Instancia o controlador de dashboard
+ 
+// Cria instanciais dos controladores para utilizar seus metodos
+$authController = new AuthController();
+$userControler = new UserController();
+$dashboard = new DashboardController();
+ 
+?>
